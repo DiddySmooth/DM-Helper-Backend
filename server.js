c3d4538d910c59c4a7ef14207a847d449ac0f1b2
Grayson McClead <gmcclead@mc02yt1kflvdl.attlocal.net>
2021-11-25 19:50:38 -0600

    trying to fix cors

--- server.js
+++ server.js
@@ -16,18 +16,16 @@ app.listen(PORT, () => {
   routesReport.print()
 })
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors({
+  origin: 'http://localhost:3000/'
+}));
 
 const userRouter = require('./routes/userRoutes')
 const genRouter = require('./routes/genRoutes')
 const magicItemsRouter = require('./routes/magicItemsRoutes')
 const spellsRouter = require('./routes/spellsRoutes')
 
-app.use('/gen', cors(), genRouter)
-app.use('/user', cors(), userRouter)
-app.use('/magicItem', cors(), magicItemsRouter)
-app.use('/spells', cors(), spellsRouter)
+app.use('/gen', genRouter)
+app.use('/user', userRouter)
+app.use('/magicItem', magicItemsRouter)
+app.use('/spells', spellsRouter)
