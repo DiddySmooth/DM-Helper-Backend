6b9e511dae3000cf24433fd8a49af44aef2adede
Grayson McClead <gmcclead@mc02yt1kflvdl.attlocal.net>
2021-11-25 20:24:00 -0600

    added cors stuff for front end and dev

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.listen(PORT, () => {
 })
 
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: ['http://localhost:3000', 'https://dmhammer.herokuapp.com']
 }));
 
 const userRouter = require('./routes/userRoutes')
