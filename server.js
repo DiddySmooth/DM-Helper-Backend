7b9b30d42856a946afceec0279206c0e465ff8d6
Grayson McClead <gmcclead@mc02yt1kflvdl.attlocal.net>
2021-11-25 21:54:59 -0600

    hopefully fixed cors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.listen(PORT, () => {
 
 app.use(cors({
   origin: 'http://localhost:3000',
-  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH','OPTIONS']
 }));
 
 //, 'https://dmhammer.herokuapp.com'
